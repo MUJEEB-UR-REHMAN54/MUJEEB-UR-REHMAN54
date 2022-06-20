0de8d4cd4578aad30f0a56ca38679ad677608caa
MUJEEB-UR-REHMAN54 <75685931+MUJEEB-UR-REHMAN54@users.noreply.github.com>
2022-06-20 15:26:27 +0500

    Add files via upload

---
+++ country.js
@@ -0,0 +1,22 @@
+country = [
+  {
+    _id: "5b21ca3eeb7f6fbccd472343",
+    name: "Portgal",
+    leagues: [
+      { _id: "5b21ca3eeb7f6fbccd471818", name: "Balarus" },
+      { _id: "5b21ca3eeb7f6fbccd471814", name: "Liga" },
+    ],
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd472345",
+    name: "India",
+    leagues: [
+      { _id: "5b21ca3eeb7f6fbccd471820", name: "English Premier League" },
+    ],
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd472347",
+    name: "China",
+    leagues: [{ _id: "5b21ca3eeb7f6fbccd471814", name: "Laliga" }],
+  },
+];
